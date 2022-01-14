b967b4ce74b7bbd5255976fadb9c22ca88e83794
umitakbas82 <umit.akbas@hotmail.com.tr>
2022-01-15 00:58:27 +0300

    added new query in business/abstact/IProductServices interface

--- Business/Abstract/IProductService.cs
+++ Business/Abstract/IProductService.cs
@@ -8,9 +8,11 @@ using System.Threading.Tasks;
 namespace Business.Abstract
 {
     public interface IProductService
-    {
+    {   
         List<Product> GetAll();
+        List<Product> GetAllByCategoryId(int id);
 
+        List<Product> GetByUnitPrice(decimal min, decimal max);
 
 
 
